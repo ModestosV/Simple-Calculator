f22fc5348ab2f87063a3580217b27e0d787ce512
Micho <michoma94@outlook.com>
2018-04-12 22:11:17 -0400

    Tabs are now linked to Hexadecimal calculator and Octodecimal calculator #Hexadecimal and Octal Conversions

--- app/src/main/kotlin/com/simplemobiletools/calculator/helpers/PageFragment.kt
+++ app/src/main/kotlin/com/simplemobiletools/calculator/helpers/PageFragment.kt
@@ -11,19 +11,28 @@ import com.simplemobiletools.calculator.R
 class PageFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.activity_binary_calculator, container, false)
+
+        var view = inflater.inflate(R.layout.activity_binary_calculator, container, false)
         val page = getArguments()!!.getInt(PAGE_NUM)
+
+        if(page == 0)
+            view = inflater.inflate(R.layout.activity_binary_calculator, container, false)
+        else if(page == 1)
+            view = inflater.inflate(R.layout.activity_octodecimal_calculator, container, false)
+        else if(page == 2)
+            view = inflater.inflate(R.layout.activity_hexadecimal_calculator, container, false)
+
         return view
     }
 
     companion object {
         val PAGE_NUM = "PAGE_NUM"
         fun newInstance(page: Int): PageFragment {
-            val partialPage = PageFragment()
+            val fragment = PageFragment()
             val args = Bundle()
             args.putInt(PAGE_NUM, page)
-            partialPage.setArguments(args)
-            return partialPage
+            fragment.setArguments(args)
+            return fragment
         }
     }
 }
